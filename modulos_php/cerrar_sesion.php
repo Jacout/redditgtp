be73642e1a489966aef397947c69822e38545ef6
Jacob Rodriguez <misaelt_99@outlook.com>
2025-05-18 14:16:22 -0600

    update index, preversion 70 por, falta hacer profile y vista para post

--- modulos_php/cerrar_sesion.php
+++ modulos_php/cerrar_sesion.php
@@ -1,7 +1,13 @@
 <?php
 
-    session_destroy();
-    header("index.php");
+    session_start(); // Inicia la sesión
+    session_unset(); // Borra las variables de sesión
+    session_destroy(); // Destruye la sesión
+    echo'<script type="text/javascript">
+        alert("Sesion cerrada, redirigiendo al inicio");
+        window.location.href="/redditgtp"
+        </script>';
+    exit();
 
 
 
--- modulos_php/create_post.php
+++ modulos_php/create_post.php
@@ -2,35 +2,19 @@
 session_start();
 include 'conex.php';
 
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit;
-}
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $title = $_POST['title'];
     $content = $_POST['content'];
     $user_id = $_SESSION['user_id'];
 
-    $stmt = $pdo->prepare("INSERT INTO posts (user_id, title, content) VALUES (?, ?, ?)");
-    $stmt->execute([$user_id, $title, $content]);
-    header("Location: index.php");
+    $stmt = $pdo->query("INSERT INTO posts (user_id, title, content) VALUES ('$user_id','$title' , '$content')");
+    //$stmt = $pdo->prepare("INSERT INTO posts (user_id, title, content) VALUES (?, ?, ?)");
+    //$stmt->execute([$user_id, $title, $content]);
+    echo'<script type="text/javascript">
+        alert("Post creado con exito");
+        window.location.href="/redditgtp"
+        </script>';
     exit;
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Crear Publicación</title>
-</head>
-<body>
-    <h1>Crear Publicación</h1>
-    <form method="POST">
-        <input type="text" name="title" placeholder="Título" required>
-        <textarea name="content" placeholder="Contenido" required></textarea>
-        <button type="submit">Publicar</button>
-    </form>
-</body>
-</html>
